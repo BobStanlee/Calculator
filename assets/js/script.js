6e264c3efb612ba7d7f5c36939d3f3bbea834746
Bob Stanley <dakotasfacts@gmail.com>
2024-01-06 14:01:36 +0000

    Added the Basic functions for a calculator
    -Addition
    -Substraction
    -Multiplication
    -Division

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+let firstNumber = 0;
+let operator = '';
+let lastNumber = 0;
+let results = 0;
+
+
+// Addition Function
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+// Subtraction Function
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+// Multiplication Function
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+// Division Function 
+function divide(num1, num2) {
+    return num1 / num2;
+}
